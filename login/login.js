a4c9e182bcc8d8feb5776306513151cbeeba7423
PNataliia <natalja.bukavyn@gmail.com>
2019-03-18 11:35:35 +0200

    start admin

--- login/login.js
+++ login/login.js
@@ -1,7 +1,7 @@
 let logincontainer = document.getElementById("logincontainer");
 let registercontainer = document.getElementById("registercontainer");
 let registertab = document.getElementById("register-tab");
-let logintab = document.getElementById("#login-tab");
+let logintab = document.getElementById("login-tab");
 let loginlink = document.getElementById(".loginlink");
 let registerlink = document.getElementById(".registerlink");
 
@@ -24,11 +24,31 @@ function loginactive (){
     registercontainer.style.display = "none";
     logintab.classList.add("active");
     registertab.classList.remove("active");
-};
+}
 
 function registeractive () {
     logincontainer.style.display = "none";
     registercontainer.style.display = "block";
     registertab.classList.add("active");
     logintab.classList.remove("active");
-};
+}
+
+
+
+
+$( document ).ready(function() {
+    var myUser = {};
+    myUser.name = "";
+    myUser.surname = "";
+    myUser.username = "";
+    myUser.password = "";
+    $("#submit-login").on("click", function () {
+        let user = JSON.stringify(myUser);
+        localStorage.setItem("user", user);
+        let userN = $("#login-username").val();
+        let pasw = $("#login-password").val();
+        if (userN === myUser.username && pasw === myUser.password) {
+            window.location.replace("adminPanel.html");
+        }
+    })
+});
