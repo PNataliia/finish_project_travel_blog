1ef459e4cd6e5f0871f6d5dbeecb939f5e3498db
PNataliia <natalja.bukavyn@gmail.com>
2019-03-20 20:28:51 +0200

    Story

--- adminpanel/adminJS.js
+++ adminpanel/adminJS.js
@@ -1,31 +1,48 @@
-$( document ).ready(function(){
+$(document).ready(function () {
     /*інпут з вибором дат*/
-let dateMemory = document.getElementById('dateToday');
+    let dateMemory = document.getElementById('dateToday');
     dateMemory.valueAsDate = new Date();
+/*stories*/
+    var allStories = [];
+    var numberStory = 0;
 
-      var allStories = [];
-    $("#story-btn").on("click", function() {
+    $("#story-btn").on("click", function () {
         let story = {};
         story.imgSRC = $("#url-img").val();
-        story.title =$("#myTitle-story").val();
-        story.text =$("#myText-story").val();
-        story.date =$("#dateToday").val();
+        story.title = $("#myTitle-story").val();
+        story.text = $("#myText-story").val();
+        story.date = $("#dateToday").val();
         allStories.push(story);
-        let storiesArray= JSON.stringify(allStories);
-        localStorage.setItem("stories",storiesArray );
+
+        let storiesArray = JSON.stringify(allStories);
+        localStorage.setItem("stories", storiesArray);
+        updateItmes();
+        alert("hbnjgjkljhg");
+    });
+    /*food dish*/
+    var allFoods = [];
+    $("#food-btn").on("click", function () {
+        let food = {};
+        food.imgSRC = $("#food-img").val();
+        food.title = $("#myTitle-food").val();
+        food.text = $("#myText-story").val();
+        food.date = $("#dateToday").val();
+        allStories.push(story);
+        let storiesArray = JSON.stringify(allStories);
+        localStorage.setItem("stories", storiesArray);
         updateItmes();
         alert("hbnjgjkljhg");
     });
 
-$("#gotoHomePage").click("click", function(){
-    window.location.replace("../index.html");
-});
+    $("#gotoHomePage").click("click", function () {
+        window.location.replace("../index.html");
+    });
 
-function updateItmes () {
-    containerStory=$("#cont").empty();
-    let storiesArray = JSON.parse(localStorage.getItem("stories"));
-    for (var i = 0; i < storiesArray.length; i++) {
-       containerStory.append("<p>" + storiesArray[i].title + "</p>")
+    function updateItmes() {
+        containerStory = $("#container-Story").empty();
+        let storiesArray = JSON.parse(localStorage.getItem("stories"));
+        for (var i = 0; i < storiesArray.length; i++) {
+            containerStory.append("<p>" + storiesArray[i].title + "</p>")
+        }
     }
-}
 });
